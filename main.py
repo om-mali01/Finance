ae893a8611a2f61a187a52a8df249dae845d61e0
om-mali01 <ommali9200@gmail.com>
2023-10-21 15:27:21 +0530

    changes in main

--- main.py
+++ main.py
@@ -94,45 +94,45 @@ def main():
     if month in data:
         existing_data = data[month]
         choice = input("\nThis month is already added !!! Do you want to update the data? (y/n): ")
-        if choice in ['y','yes','Yes',"YES"]:
-            existing_data = data[month]
-            existing_income = existing_data['income']
-            existing_expenses = existing_data['expenses']
-            
-            # Check if the existing income and expenses match the new input
-            if existing_income == income and existing_expenses == expenses:
-                print("Data for this month already exists and is up to date.")
-            else:
-                print("Updating existing data for this month.")
-                data[month]['income'] = income
-                data[month]['expenses'] = expenses
-                remaining_amount = income - expenses
-                data[month]['remaining_amount'] = remaining_amount
-                bucket, bucket_total = bucket_info(remaining_amount)
-                data[month]['bucket'] = bucket
-                data[month]['bucket_total'] = bucket_total
-                data[month]['Gross Saved'] = remaining_amount - bucket_total
-                save_data(data, filename)
-                print_info(month, income, expenses, remaining_amount, data[month]['bucket'], data[month]['bucket_total'])
-                data_year(data)
-        else:
+        if choice not in ['y','yes','Yes',"YES"]:
             print_info(month, existing_data['income'], existing_data['expenses'], existing_data['remaining_amount'], existing_data['bucket'], existing_data['bucket_total'])
             data_year(data)
-    else:
-        remaining_amount = income - expenses
-        bucket, bucket_total = bucket_info(remaining_amount)
-        data[month] = {
-                'month' : month,
-                'income' : income,
-                'expenses' : expenses,
-                'remaining_amount':remaining_amount,
-                'bucket':bucket,
-                'bucket_total':bucket_total,
-                'Gross Saved':remaining_amount - bucket_total
-        }
-        save_data(data, filename)
-        print_info(month, income, expenses, remaining_amount, bucket, bucket_total)
-        data_year(data)
+            return
+        
+        existing_data = data[month]
+        existing_income = existing_data['income']
+        existing_expenses = existing_data['expenses']
+            # Check if the existing income and expenses match the new input
+        if existing_income == income and existing_expenses == expenses:
+            print("Data for this month already exists and is up to date.")
+        else:
+            print("Updating existing data for this month.")
+            data[month]['income'] = income
+            data[month]['expenses'] = expenses
+            remaining_amount = income - expenses
+            data[month]['remaining_amount'] = remaining_amount
+            bucket, bucket_total = bucket_info(remaining_amount)
+            data[month]['bucket'] = bucket
+            data[month]['bucket_total'] = bucket_total
+            data[month]['Gross Saved'] = remaining_amount - bucket_total
+            save_data(data, filename)
+            print_info(month, income, expenses, remaining_amount, data[month]['bucket'], data[month]['bucket_total'])
+            data_year(data)
+
+    remaining_amount = income - expenses
+    bucket, bucket_total = bucket_info(remaining_amount)
+    data[month] = {
+            'month' : month,
+            'income' : income,
+            'expenses' : expenses,
+            'remaining_amount':remaining_amount,
+            'bucket':bucket,
+            'bucket_total':bucket_total,
+            'Gross Saved':remaining_amount - bucket_total
+    }
+    save_data(data, filename)
+    print_info(month, income, expenses, remaining_amount, bucket, bucket_total)
+    data_year(data)
 
 if __name__ == "__main__":
     main()
